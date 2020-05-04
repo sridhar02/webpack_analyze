8418d4b571d0f37811336e1a783dfa05effd49bb
sridhar02 <kattasridhar02@gmail.com>
2020-05-04 10:25:40 +0530

    navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Link,
+  makeStyles,
+} from "@material-ui/core";
+
+const useNavbarStyles = makeStyles({
+  text: {
+    color: "white",
+    marginRight: "25px",
+  },
+});
+
+export default function Navbar() {
+  const classes = useNavbarStyles();
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>
+          <Typography>
+            <Link href="/open" className={classes.text}>
+              Open
+            </Link>
+          </Typography>
+          <Typography>
+            <Link href="/Home" className={classes.text}>
+              Home
+            </Link>
+          </Typography>
+          <Typography>
+            <Link href="/Modules" className={classes.text}>
+              Modules
+            </Link>
+          </Typography>
+          <Typography>
+            <Link href="/chunks" className={classes.text}>
+              Chunks
+            </Link>
+          </Typography>
+          <Typography>
+            <Link href="/assets" className={classes.text}>
+              Assets
+            </Link>
+          </Typography>
+          <Typography>
+            <Link href="/Home" className={classes.text}>
+              Warnings
+            </Link>
+          </Typography>
+          <Typography>
+            <Link href="/Home" className={classes.text}>
+              Errors
+            </Link>
+          </Typography>
+          <Typography>
+            <Link href="/Home" className={classes.text}>
+              Hints
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
