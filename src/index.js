586a9caaf19465ca4c97bf5a47e0a0f348c38c8b
sridhar02 <kattasridhar02@gmail.com>
2020-05-04 12:07:58 +0530

    Remaining routes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import Assets from "./components/assets";
 import Modules from "./components/modules";
 
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Warnings from "./components/warnings";
+import Errors from "./components/errors";
+import Hints from "./components/hints";
 
 function Main() {
   return (
@@ -18,6 +21,9 @@ function Main() {
         <Route path="/Modules" component={Modules} exact />
         <Route path="/chunks" component={Chunks} exact />
         <Route path="/assets" component={Assets} exact />
+        <Route path="/warnings" component={Warnings} exact />
+        <Route path="/errors" component={Errors} exact />
+        <Route path="/hints" component={Hints} exact />
       </Switch>
     </Router>
   );
