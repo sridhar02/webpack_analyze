334fccbcd7411cf8e120e71e138cd585124c920a
sridhar02 <kattasridhar02@gmail.com>
2020-05-04 10:25:03 +0530

    Add home and module routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import App from "./App";
+import Home from "./components/home";
+import Modules from "./components/modules";
+
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 function Main() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={App} />
+        <Route path="/" component={App} exact />
+        <Route path="/Home" component={Home} exact />
+        <Route path="/Modules" component={Modules} exact />
       </Switch>
     </Router>
   );
