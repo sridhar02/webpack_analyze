9965b4008ff695c055b68fd1fb33e6b0b11e596b
sridhar02 <kattasridhar02@gmail.com>
2020-05-28 14:30:03 +0530

    Module component

---
+++ src/components/Module.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Navbar from "./navbar";
+const example = require("../example1.json");
+
+export default function Module() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
